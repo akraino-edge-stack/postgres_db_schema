d6bbb9ad21e8007ef6ac978fee27ffab8e8690e0
Lakshminarayana Devaraj <ld261v@att.com>
2018-08-01 15:09:11 -0400

    portal db update for onap
    Change-Id: I6d4aec831c4eb36483fa7ee59fa9a655c600b8e5
    Signed-off-by: Lakshminarayana Devaraj <ld261v@att.com>

--- akraino-db_0524.sql
+++ akraino-db_0524.sql
@@ -91,7 +91,7 @@ CREATE TABLE akraino.edge_site
    edge_site_user text NULL, 
    edge_site_pwd text NULL, 
    edge_site_blueprint text NULL,
-   input_file bytea NOT NULL, 
+   input_file bytea NULL, 
    output_yaml_file_1 bytea NULL,
    output_yaml_file_2 bytea NULL,
    output_yaml_file_3 bytea NULL,
@@ -168,17 +168,18 @@ CREATE TABLE akraino.onap
 (
    onap_id bigint not NULL, 
    edge_site_name text NOT NULL, 
-   public_net_name text not NULL, 
-   public_subnet_cidr text not NULL, 
-   public_subnet_allocation_start text not NULL, 
-   public_subnet_allocation_end text not null, 
-   public_subnet_dns_nameserver text not NULL, 
-   public_subnet_gateway_ip text not NULL, 
-   onap_vm_public_key text not NULL, 
-   onap_repo text not null, 
-   http_proxy text not NULL, 
-   https_proxy text not NULL, 
-   no_proxy text not null,
+   input_file bytea NULL,
+   public_net_name text NULL, 
+   public_subnet_cidr text NULL, 
+   public_subnet_allocation_start text NULL, 
+   public_subnet_allocation_end text null, 
+   public_subnet_dns_nameserver text NULL, 
+   public_subnet_gateway_ip text NULL, 
+   onap_vm_public_key text NULL, 
+   onap_repo text null, 
+   http_proxy text NULL, 
+   https_proxy text NULL, 
+   no_proxy text null,
    CONSTRAINT onap_id_pk PRIMARY KEY (onap_id)
 ) 
 WITH (
@@ -193,6 +194,9 @@ CREATE SEQUENCE akraino.seq_pod
 
   CREATE SEQUENCE akraino.seq_rack
   START WITH 1 INCREMENT BY 1;
+  
+  CREATE SEQUENCE akraino.seq_onap
+  START WITH 1 INCREMENT BY 1;
 
 insert into akraino.region values(1, 'US Northeast', now(), user, now(), user);
 
@@ -200,27 +204,49 @@ insert into akraino.edge_site( edge_site_id, edge_site_name, crt_login_id, crt_d
 insert into akraino.edge_site( edge_site_id, edge_site_name,  crt_login_id, crt_dt, upd_login_id, upd_dt, region_id) values(2, 'MTN2', user,  now(), user, now(),1);
 
 
-insert into akraino.edge_site_input_yaml_files values (1, 'mycontrolplane_hp', '/profiles/host/',bytea(pg_read_file('profiles/host/mycontrolplane_hp.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (2, 'generic', '/profiles/hardware/',bytea(pg_read_file('profiles/hardware/generic.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (3, 'region', '/profiles/',bytea(pg_read_file('profiles/region.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (4, 'bootaction', '/baremetal/',bytea(pg_read_file('baremetal/bootaction.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (5, 'localadmin_ssh_public_key', '/secrets/publickey/',bytea(pg_read_file('secrets/publickey/localadmin_ssh_public_key.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (6, 'ipmi_admin_password', '/secrets/passphrases/',bytea(pg_read_file('secrets/passphrases/ipmi_admin_password.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (7, 'ceph-update', '/software/charts/ucp/ceph/',bytea(pg_read_file('software/charts/ucp/ceph/ceph-update.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (8, 'ingress', '/software/charts/kubernetes/ingress/',bytea(pg_read_file('software/charts/kubernetes/ingress/ingress.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (9, 'calico', '/software/charts/kubernetes/container-networking/',bytea(pg_read_file('software/charts/kubernetes/container-networking/calico.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (10, 'neutron', '/software/charts/osh/openstack-compute-kit/',bytea(pg_read_file('software/charts/osh/openstack-compute-kit/neutron.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (11, 'ipmi_admin_password', '/aic-clcp-security-manifests/secrets/passphrases/',bytea(pg_read_file('aic-clcp-security-manifests/secrets/passphrases/ipmi_admin_password.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (12, 'site-definition', '/aic-clcp-security-manifests/',bytea(pg_read_file('aic-clcp-security-manifests/site-definition.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (13, 'common-addresses', '/networks/',bytea(pg_read_file('networks/common-addresses.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (14, 'pki-catalog', '/pki/',bytea(pg_read_file('pki/pki-catalog.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (15, 'ceph', '/software/charts/ucp/ceph/',bytea(pg_read_file('software/charts/ucp/ceph/ceph.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (16, 'nova', '/software/charts/osh/openstack-compute-kit/',bytea(pg_read_file('software/charts/osh/openstack-compute-kit/nova.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (17, 'promenade', '/software/charts/ucp/ceph/promenade/',bytea(pg_read_file('software/charts/ucp/ceph/promenade/promenade.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (18, 'etcd', '/software/charts/kubernetes/etcd/',bytea(pg_read_file('software/charts/kubernetes/etcd/etcd.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (19, 'rack', '/baremetal/',bytea(pg_read_file('baremetal/rack.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (20, 'etcd', '/software/charts/kubernetes/container-networking/',bytea(pg_read_file('software/charts/kubernetes/container-networking/etcd.j2')), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (21, 'rack', '/networks/physical/',bytea(pg_read_file('networks/physical/rack.j2')), now(), user, now(), user);
+insert into akraino.edge_site_input_yaml_files values (1, 'site-definition', '/',bytea(pg_read_file('site-definition.j2')), now(), user, now(), user);
+
+
+insert into akraino.edge_site_input_yaml_files values (2, 'bootaction-sriov-blacklist', '/baremetal/', bytea(pg_read_file('baremetal/bootaction-sriov-blacklist.j2')), now(), user, now(), user);
+--insert into akraino.edge_site_input_yaml_files values (3, 'calico-ip-rules', '/baremetal/',bytea(pg_read_file('baremetal/calico-ip-rules.j2')), now(), user, now(), user);
+insert into akraino.edge_site_input_yaml_files values (4, 'promjoin', '/baremetal/',bytea(pg_read_file('baremetal/promjoin.j2')), now(), user, now(), user);
+insert into akraino.edge_site_input_yaml_files values (5, 'rack', '/baremetal/',bytea(pg_read_file('baremetal/rack.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (6, 'common-addresses', '/networks/',bytea(pg_read_file('networks/common-addresses.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (7, 'rack', '/networks/physical/',bytea(pg_read_file('networks/physical/rack.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (8, 'pki-catalog', '/pki/',bytea(pg_read_file('pki/pki-catalog.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (9, 'region', '/profiles/',bytea(pg_read_file('profiles/region.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (10, 'generic', '/profiles/hardware/',bytea(pg_read_file('profiles/hardware/generic.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (11, 'compute-r01', '/profiles/host/',bytea(pg_read_file('profiles/host/compute-r01.j2')), now(), user, now(), user);
+insert into akraino.edge_site_input_yaml_files values (12, 'cp-r01', '/profiles/host/',bytea(pg_read_file('profiles/host/cp-r01.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (13, 'ipmi_admin_password', '/secrets/passphrases/',bytea(pg_read_file('secrets/passphrases/ipmi_admin_password.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (14, 'localadmin_ssh_public_key', '/secrets/publickey/',bytea(pg_read_file('secrets/publickey/localadmin_ssh_public_key.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (15, 'calico', '/software/charts/kubernetes/container-networking/',bytea(pg_read_file('software/charts/kubernetes/container-networking/calico.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (16, 'etcd', '/software/charts/kubernetes/container-networking/',bytea(pg_read_file('software/charts/kubernetes/container-networking/etcd.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (17, 'etcd', '/software/charts/kubernetes/etcd/',bytea(pg_read_file('software/charts/kubernetes/etcd/etcd.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (18, 'ingress', '/software/charts/kubernetes/ingress/',bytea(pg_read_file('software/charts/kubernetes/ingress/ingress.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (19, 'neutron', '/software/charts/osh/openstack-compute-kit/',bytea(pg_read_file('software/charts/osh/openstack-compute-kit/neutron.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (20, 'nova', '/software/charts/osh/openstack-compute-kit/',bytea(pg_read_file('software/charts/osh/openstack-compute-kit/nova.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (21, 'ceph-update', '/software/charts/ucp/ceph/',bytea(pg_read_file('software/charts/ucp/ceph/ceph-update.j2')), now(), user, now(), user);
+
+insert into akraino.edge_site_input_yaml_files values (22, 'ceph', '/software/charts/ucp/ceph/',bytea(pg_read_file('software/charts/ucp/ceph/ceph.j2')), now(), user, now(), user);
+
+
+insert into akraino.edge_site_input_yaml_files values (23, 'promenade', '/software/charts/ucp/promenade/',bytea(pg_read_file('software/charts/ucp/promenade/promenade.j2')), now(), user, now(), user);
 
 
 commit;
