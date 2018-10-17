836a4e484729a3bf0a8c74b54c087b038a23f13a
Lakshminarayana Devaraj <ld261v@att.com>
2018-10-17 16:40:27 -0400

    multi-node changes
    Change-Id: Ia3782932e366202cfa7ef28868fc1df61216b62d
    Signed-off-by: Lakshminarayana Devaraj <ld261v@att.com>

--- akraino-db_0524.sql
+++ akraino-db_0524.sql
@@ -24,6 +24,7 @@ drop sequence IF EXISTS akraino.seq_hardware;
 drop sequence IF EXISTS akraino.seq_onap;
 drop sequence IF EXISTS akraino.seq_blueprint;
 drop sequence IF EXISTS akraino.seq_edgeNode;
+drop sequence IF EXISTS akraino.seq_yamltemplate;
 
 drop table IF EXISTS akraino.onap;
 drop table IF EXISTS akraino.usersession;
@@ -35,7 +36,7 @@ drop table IF EXISTS akraino.genericrack;
 drop table IF EXISTS akraino.software;
 drop table IF EXISTS akraino.hardware;
 drop table IF EXISTS akraino.pod;
-drop table IF EXISTS akraino.edge_site_input_yaml_files;
+drop table IF EXISTS akraino.edge_site_template_file;
 drop table IF EXISTS akraino.edge_site;
 drop table IF EXISTS akraino.region;
  
@@ -243,11 +244,13 @@ CREATE TABLE akraino.edge_site
    output_yaml_file_21 bytea NULL,
    output_yaml_file_22 bytea NULL,
    output_yaml_file_23 bytea NULL,
+   output_yaml_file_24 bytea NULL,
    build_status text NULL, 
    build_dt timestamptz NULL,
-   deploy_createtar_status text NULL,    
-   deploy_genesisnodestatus_status text NULL,    
-   deploy_deploytoolstatus_status text NULL,    
+   multinode_createtar_status text NULL,    
+   multinode_genesisnode_status text NULL,    
+   multinode_deploytool_status text NULL,  
+   multinode_transferfile_status text NULL, 
    deploy_status text NULL,  
    onap_status text NULL,
    tempest_status text NULL,
@@ -271,24 +274,24 @@ WITH (
 ALTER TABLE akraino.edge_site
   OWNER TO postgres;
   
- 
-CREATE TABLE akraino.edge_site_input_yaml_files
+
+CREATE TABLE akraino.edge_site_template_file
 (   
-   id bigint NOT NULL,
-   input_yaml_file_name text NOT NULL,
-   input_yaml_file_location text NOT NULL,
-   input_yaml_file_content bytea NOT NULL,
-   crt_dt timestamptz NOT NULL, 
-   crt_login_id text NOT NULL, 
-   upd_dt timestamptz NOT NULL, 
-   upd_login_id text NOT NULL,
-	CONSTRAINT edge_site_yaml_id_pk PRIMARY KEY (id)   
+   yamltemplate_id bigint NOT NULL,
+   template_file_name text NOT NULL,
+   template_file_location text NOT NULL,
+   template_file_content bytea NOT NULL,
+   crt_dt timestamptz NULL, 
+   crt_login_id text NULL, 
+   upd_dt timestamptz NULL, 
+   upd_login_id text NULL,
+   CONSTRAINT edge_site_yaml_id_pk PRIMARY KEY (yamltemplate_id)   
 )
 WITH (
   OIDS = FALSE
 )
 ;
-ALTER TABLE akraino.edge_site_input_yaml_files
+ALTER TABLE akraino.edge_site_template_file
   OWNER TO postgres;
 ;
 
@@ -346,60 +349,15 @@ CREATE SEQUENCE akraino.seq_blueprint
   
 CREATE SEQUENCE akraino.seq_edgeNode
   START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE akraino.seq_yamltemplate
+  START WITH 1 INCREMENT BY 1;  
+  
   
 insert into akraino.region values(1, 'US Northeast', now(), user, now(), user);
 
 insert into akraino.edge_site( edge_site_id, edge_site_name, crt_login_id, crt_dt, upd_login_id, upd_dt, region_id) values(1, 'MTN1', user,  now(), user, now(),1);
 insert into akraino.edge_site( edge_site_id, edge_site_name,  crt_login_id, crt_dt, upd_login_id, upd_dt, region_id) values(2, 'MTN2', user,  now(), user, now(),1);
-
-
-insert into akraino.edge_site_input_yaml_files values (1, 'site-definition', '/', convert_to(pg_read_file('site-definition.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (2, 'bootaction-sriov-blacklist', '/baremetal/', convert_to(pg_read_file('baremetal/bootaction-sriov-blacklist.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (3, 'calico-ip-rules', '/baremetal/', convert_to(pg_read_file('baremetal/calico-ip-rules.j2'), 'utf-8'), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (4, 'promjoin', '/baremetal/', convert_to(pg_read_file('baremetal/promjoin.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (5, 'rack', '/baremetal/', convert_to(pg_read_file('baremetal/rack.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (6, 'common-addresses', '/networks/', convert_to(pg_read_file('networks/common-addresses.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (7, 'rack', '/networks/physical/', convert_to(pg_read_file('networks/physical/rack.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (8, 'pki-catalog', '/pki/', convert_to(pg_read_file('pki/pki-catalog.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (9, 'region', '/profiles/', convert_to(pg_read_file('profiles/region.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (10, 'generic', '/profiles/hardware/', convert_to(pg_read_file('profiles/hardware/generic.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (11, 'compute-r01', '/profiles/host/',  convert_to(pg_read_file('profiles/host/compute-r01.j2'), 'utf-8'), now(), user, now(), user);
-insert into akraino.edge_site_input_yaml_files values (12, 'cp-r01', '/profiles/host/', convert_to(pg_read_file('profiles/host/cp-r01.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (13, 'ipmi_admin_password', '/secrets/passphrases/', convert_to(pg_read_file('secrets/passphrases/ipmi_admin_password.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (14, 'localadmin_ssh_public_key', '/secrets/publickey/', convert_to(pg_read_file('secrets/publickey/localadmin_ssh_public_key.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (15, 'calico', '/software/charts/kubernetes/container-networking/', convert_to(pg_read_file('software/charts/kubernetes/container-networking/calico.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (16, 'etcd', '/software/charts/kubernetes/container-networking/', convert_to(pg_read_file('software/charts/kubernetes/container-networking/etcd.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (17, 'etcd', '/software/charts/kubernetes/etcd/', convert_to(pg_read_file('software/charts/kubernetes/etcd/etcd.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (18, 'ingress', '/software/charts/kubernetes/ingress/', convert_to(pg_read_file('software/charts/kubernetes/ingress/ingress.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (19, 'neutron', '/software/charts/osh/openstack-compute-kit/', convert_to(pg_read_file('software/charts/osh/openstack-compute-kit/neutron.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (20, 'nova', '/software/charts/osh/openstack-compute-kit/', convert_to(pg_read_file('software/charts/osh/openstack-compute-kit/nova.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (21, 'ceph-client-update', '/software/charts/ucp/ceph/', convert_to(pg_read_file('software/charts/ucp/ceph/ceph-client-update.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (22, 'ceph-client', '/software/charts/ucp/ceph/', convert_to(pg_read_file('software/charts/ucp/ceph/ceph-client.j2'), 'utf-8'), now(), user, now(), user);
-
-insert into akraino.edge_site_input_yaml_files values (23, 'ceph-osd', '/software/charts/ucp/ceph/', convert_to(pg_read_file('software/charts/ucp/ceph/ceph-osd.j2'), 'utf-8'), now(), user, now(), user);
-
-
-insert into akraino.edge_site_input_yaml_files values (24, 'promenade', '/software/charts/ucp/promenade/', convert_to(pg_read_file('software/charts/ucp/promenade/promenade.j2'), 'utf-8'), now(), user, now(), user);
-
 
 commit;
 
